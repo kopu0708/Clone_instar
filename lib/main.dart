b5faf49002fdc5204c6db3950d56bd6cbddc14e3
kopu0708 <ghop0822@gmail.com>
2025-09-23 16:05:15 +0900

    몇번째 인지 까먹음

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,19 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/rendering.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-      MaterialApp(
-       theme: style.theme,
-        home: MyApp(),
+      ChangeNotifierProvider(
+          create: (c) => Store1(),
+          child: MaterialApp(
+            theme: style.theme,
+            home: MyApp(),
+          )
       )
   );
 }
@@ -31,22 +37,41 @@ class _MyAppState extends State<MyApp> {
   var isLoading = false;
   var userImage;
 
-  getData() async{
-  var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
-  if(result.statusCode == 200){
-    setState(() {
-      data = (jsonDecode(result.body));
-    });
-    print(data);
+  saveData() async{
+    var storage = await SharedPreferences.getInstance(); //저장공간 오픈하는 법
+    storage.setString('post_data', jsonEncode(data));
   }
-  else{
+
+  loadData() async{
+    var storage = await SharedPreferences.getInstance();
+    var result = storage.getString('post_data');
+
+    if(result != null){
+      setState(() {
+        data = jsonDecode(result);
+      });
+    }
+    else {
+      getData();
+    }
+  }
+
+  getData() async{
+    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+    if(result.statusCode == 200){
+      setState(() {
+        data = (jsonDecode(result.body));
+      });
+      print(data);
+      saveData();
+    }
+    else{
       Fluttertoast.showToast(
         msg: "데이터를 불러오는데 실패했습니다.",
         toastLength: Toast.LENGTH_SHORT,
         gravity: ToastGravity.BOTTOM,);
-    //throw Exception('실패함 ㅄ');
+    }
   }
-}
   getmore() async{
     if (isLoading) return;
 
@@ -61,6 +86,7 @@ class _MyAppState extends State<MyApp> {
         data.add(moreData);
         page++;
       });
+      saveData();
     }
     else{
       Fluttertoast.showToast(
@@ -75,12 +101,13 @@ class _MyAppState extends State<MyApp> {
   void addPost(Map<String, dynamic> newPost){
     setState(() {
       data.insert(0, newPost);
+      saveData();
     });
   }
   @override
   void initState() {
     super.initState();
-    getData();
+    loadData();
   }
 
   @override
@@ -89,25 +116,28 @@ class _MyAppState extends State<MyApp> {
       appBar: AppBar(
         title:Text('Instargram'),
         actions: [
-        IconButton(
+          IconButton(
             icon: Icon(Icons.add_box_outlined),
             onPressed: () async{
               var picker = ImagePicker();
               var image = await picker.pickImage(source: ImageSource.gallery);
-              if(image != null){
+              if(image != null) {
                 setState(() {
                   userImage = File(image.path);
                 });
+                
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (c) =>
+                        Upload(
+                          userImage: userImage,
+                          addPost: addPost,
+                          data: data,
+                        ))
+                );
               }
-              Navigator.push(context,
-              MaterialPageRoute(builder: (c) => Upload(
-                  userImage: userImage,
-                  addPost: addPost,
-              ))
-              );
             },
-        )
-      ],
+          )
+        ],
       ),
       body: [Home(data: data, getmore : getmore), Text('shop')][tab],
       bottomNavigationBar: BottomNavigationBar(
@@ -140,7 +170,7 @@ class _HomeState extends State<Home> {
 
   var scroll = ScrollController();
   var extraData;
-  
+
   @override
   void initState() {
     super.initState();
@@ -150,7 +180,7 @@ class _HomeState extends State<Home> {
       }
     });
   }
-  
+
 
   @override
   Widget build(BuildContext context) {
@@ -159,30 +189,33 @@ class _HomeState extends State<Home> {
           itemCount: widget.data.length,
           controller: scroll,
           itemBuilder: (c,i){
-            Widget imageWidget;
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start ,
+              children: [
 
-            var imageData = widget.data[i]['image'];
-            // 이미지 데이터의 타입을 확인
-            if (imageData is String) {
-              // 타입이 String이면 Image.network 사용
-              imageWidget = Image.network(imageData);
-            } else if (imageData is File) {
-              // 타입이 File이면 Image.file 사용
-              imageWidget = Image.file(imageData);
-            } else {
-              // 예외 처리: 혹시 모를 다른 타입에 대비해 빈 컨테이너 표시
-              imageWidget = Container(color: Colors.grey);
-            }
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start ,
-          children: [
-            imageWidget,
-            Text('좋아용 ${widget.data[i]['likes'].toString()}'),
-            Text(widget.data[i]['user'].toString()),
-            Text(widget.data[i]['content'].toString()),
-          ],
-        );
-      }
+                widget.data[i]['image'].runtimeType == String
+                    ? Image.network(widget.data[i]['image'])
+                    : Image.file(widget.data[i]['image']),
+
+                GestureDetector(
+                  child: Text(widget.data[i]['user']),
+                  onTap: (){
+                    Navigator.push(context,
+                        PageRouteBuilder(
+                            pageBuilder: (c,a1,a2) => Profile(),
+                            transitionsBuilder: (c,a1,a2,child)=>
+                                FadeTransition(opacity: a1, child: child),
+                            transitionDuration: Duration( milliseconds: 500)
+                        )
+                    );
+                  },
+                ),
+                Text('좋아용 ${widget.data[i]['likes'].toString()}'),
+                Text(widget.data[i]['user'].toString()),
+                Text(widget.data[i]['content'].toString()),
+              ],
+            );
+          }
       );
     }
     else {
@@ -191,9 +224,10 @@ class _HomeState extends State<Home> {
   }
 }
 class Upload extends StatelessWidget {
-  const Upload({super.key,this.userImage, this.addPost});
+  const Upload({super.key,this.userImage, this.addPost, required this.data});
   final userImage;
   final Function(Map<String, dynamic>)? addPost;
+  final List data;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -205,15 +239,16 @@ class Upload extends StatelessWidget {
           Text('이미지 업로드 화면'),
           TextButton(onPressed: (){
             Navigator.push(
-              context, 
-              MaterialPageRoute(builder: (context) => (addList(
-                userImage: userImage,
-                addPost: addPost,
-              )))
+                context,
+                MaterialPageRoute(builder: (context) => (addList(
+                  userImage: userImage,
+                  addPost: addPost,
+                  data: data,
+                )))
             );
           }, child: Text('다음')),
           IconButton(onPressed: (){Navigator.pop(context);},
-                     icon: Icon(Icons.close)
+              icon: Icon(Icons.close)
           )
         ],
       ),
@@ -221,16 +256,17 @@ class Upload extends StatelessWidget {
   }
 }
 class addList extends StatefulWidget {
-   addList({super.key, this.userImage, this.addPost});
-   final userImage;
-   final Function(Map<String,dynamic>)? addPost;
+  addList({super.key, this.userImage, this.addPost,required this.data});
+  final userImage;
+  final Function(Map<String,dynamic>)? addPost;
+  final List data;
   @override
   State<addList> createState() => _addListState();
 }
 
 class _addListState extends State<addList> {
-   final UserName = TextEditingController();
-   final letter = TextEditingController();
+  final UserName = TextEditingController();
+  final letter = TextEditingController();
 
   @override
   void dispose(){
@@ -258,15 +294,15 @@ class _addListState extends State<addList> {
             keyboardType: TextInputType.multiline,
             minLines: 5, maxLines: 10, maxLength: 100,
             decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: '글내용 작성'
+                border: OutlineInputBorder(),
+                hintText: '글내용 작성'
             ),
           ),
           ElevatedButton(onPressed: (){
             if(widget.addPost != null){
               var newPost = {
-                "id": UserName,
-                "image": widget.userImage, // File 객체 이미지
+                "id": widget.data.length,
+                "image": widget.userImage.path, // File 객체 이미지
                 "likes": 0,
                 "date": "July 25",
                 "content": letter.text,
@@ -277,15 +313,63 @@ class _addListState extends State<addList> {
             }
             Navigator.of(context).popUntil((route) => route.isFirst);
           },
-              child:Text('발행'),
-              style: ElevatedButton.styleFrom(
+            child:Text('발행'),
+            style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.lightBlueAccent,
                 foregroundColor: Colors.black,
                 elevation: 5
-              ),
+            ),
           )
         ],),
       ),
+    );
+  }
+}
+class Store1 extends ChangeNotifier{
+  var name = 'Profile';
+  var follow = 0;
+  bool isfollow = false;
+
+  FollowerUpDown(){
+    if(isfollow == false){
+    follow++;
+    isfollow = true;
+    notifyListeners();
+    }
+    else{
+      follow--;
+      isfollow = false;
+      notifyListeners();
+    }
+  }
+}
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(context.watch<Store1>().name),),
+      body: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.grey,
+              ),
+              Text('팔로워 ${context.watch<Store1>().follow}명'),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: context.read<Store1>().isfollow ? Colors.white70 : Colors.blue
+                ),
+                  onPressed: (){
+                context.read<Store1>().FollowerUpDown();
+              },
+                  child: Text('Follow')
+              )
+            ],
+          )
     );
   }
 }
